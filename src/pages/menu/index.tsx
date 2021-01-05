c178b6c767ebdb67dc3e45666f246e0e83fa9d42
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-01-05 09:43:59 -0300

    Adding Menu Page

---
+++ src/pages/menu/index.tsx
@@ -0,0 +1,14 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { Background, Title, Subtitle, Info, Button } from './styles'
+import InputFild from '../../components/InputFild'
+import Image from 'next/image'
+
+const Menu: React.FC<AppProps> = () => {
+  return (
+      <Background>
+        
+      </Background>
+    )
+}
+
+export default Menu
---
+++ src/pages/menu/styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+const Background = styled.main`
+  width: 100%;
+  height: 100vh;
+  background-color: #2E65C6;
+`
+
+const Title = styled.h1`
+  color: white;
+  text-align: center;
+  padding: ${props => props.theme.field.gap.big};
+  padding-bottom: 0;
+  font-size: ${props => props.theme.font.size.regular};
+  font-weight: ${props => props.theme.font.weight.regular};
+`
+
+const Subtitle = styled.h2`
+  width: ${props => props.theme.field.width};
+  padding-bottom: 1.5em;
+  font-size: ${props => props.theme.font.size.regular};
+  font-weight: ${props => props.theme.font.weight.medium};
+`
+
+const Info = styled.p`
+  text-align: center;
+  width: ${props => props.theme.field.width};
+  margin: ${props => props.theme.field.gap.medium};
+  font-size: ${props => props.theme.font.size.regular};
+  font-weight: ${props => props.theme.font.weight.small};
+`
+
+const Button = styled.button`
+  width: 6em;
+  height: 6em;
+  border-radius: ${props => props.theme.rounded};
+  background-color: #2E65C6;
+  box-shadow: 0px 0px 10px rgba(39, 90, 181, 0.5);
+`
+
+export {
+  Background,
+  Title,
+  Subtitle,
+  Info,
+  Button
+}
