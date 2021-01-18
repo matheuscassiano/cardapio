ddeae824f3bf074a4c9310b6ab6b2a90d0ab9649
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-01-18 16:35:06 -0300

    Re-styling Tray Bar

--- src/components/Footer/index.tsx
+++
@@ -1,8 +0,0 @@
-import Image from 'next/image';
-import FooterContainer from './styles';
-
-const Footer: React.FC<ListItem> = ({ children }) => (
-  <FooterContainer>{children}</FooterContainer>
-)
-
-export default Footer
---
+++ src/components/Tray/index.tsx
@@ -0,0 +1,7 @@
+import TrayContainer from './styles';
+
+const Tray: React.FC = ({ children }) => (
+  <TrayContainer>{children}</TrayContainer>
+)
+
+export default Tray
